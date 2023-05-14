c3074650c0d9f0b907a618ccaabeaab4c95383a4
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-05-15 00:58:37 +0530

    fix: remove nvim configs

--- .config/nvim/ftplugin/cpp.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("BufWritePost", {
-	pattern = { "*.cpp" },
-	callback = function()
-		vim.api.nvim_command("!g++ % -o %:r.out")
-		-- vim.api.nvim_command("silent !g++ % -o %:p")
-	end,
-})
