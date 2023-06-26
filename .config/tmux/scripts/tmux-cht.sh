1ad59aa8e8c7622e9387259f11526400ca4e7d15
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-06-26 19:37:42 +0530

    fix: tmuxsessionizer and tmux cht.sh

--- .config/tmux/scripts/tmux-cht.sh
+++ .config/tmux/scripts/tmux-cht.sh
@@ -1,13 +1,72 @@
 #!/usr/bin/env bash
-selected=`cat ~/.config/tmux/scripts/.tmux-cht-languages ~/.config/tmux/scripts/.tmux-cht-command | fzf`
+
+languages="c
+cpp
+java
+lua
+python
+sh
+bash
+zsh
+tmux
+html
+css
+php
+nodejs
+react
+typescript
+javascript
+next
+tailwind
+go/go
+rust"
+
+commands="find
+man
+tldr
+sed
+awk
+tr
+cp
+ls
+grep
+xargs
+rg
+ps
+mv
+kill
+lsof
+less
+head
+tar
+cp
+rm
+rename
+jq
+cat
+ssh
+cargo
+git
+git-worktree
+git-status
+git-commit
+git-rebase
+docker
+docker-compose
+stow
+chmod
+chown
+make"
+
+selected=$(echo -e "$languages\n$commands" | fzf)
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.config/tmux/scripts/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
+if grep -qs "$selected" <<< "$languages"; then
+    query=$(echo "$query" | tr ' ' '+')
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
